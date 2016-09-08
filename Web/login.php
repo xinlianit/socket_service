9f49fff122922b9844fdf90000a410784b8b4525
JiRY <390066398@qq.com>
2016-09-08 10:18:22 +0800

    添加第三方插件

---
+++ Web/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>账户登录</title>
+<style type="text/css">
+.main{border:1px solid #d7d7d7;}
+.main ul{list-style:none;margin:0px;padding:0px;}
+.main ul .list{padding:10px;}
+.listbr{border-top:1px solid #D7D7D7;}
+.main ul .list .left,.main ul .list .center,.main ul .list .right{display:inline-block;margin:0px;height:45px;line-height:45px;}
+.main ul .list .left{width:24%;}
+.main ul .list .center{width:20%;text-align:center;color:red;}
+.main ul .list .right{width:50%;text-align:right;}
+.main ul .list h3{margin:0px;}
+.tips{float:left;line-height:20px;margin-top:-15px;}
+</style>
+</head>
+<body>
+	<div class="main">
+		<form action="" method="post">
+			<ul id="list_c">
+				<li>
+					<span>商户ID：</span>
+					<input type="text" name="shop_code" value="" />
+				</li>
+			</ul>
+		</form>
+	</div>
+</body>
+</html>
